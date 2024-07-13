6994ace74f2bb54e04b5797de4cb31e9025739f4
Rich Clingman <richclingman@gmail.com>
2024-07-13 11:25:01 -0500

    1900 - 2020 song lists

--- scripts/scrape-songs.js
+++ scripts/scrape-songs.js
@@ -105,6 +105,8 @@ async function scrapeTopSongs(genre, year) {
 
     const genres = ['top-100-songs', 'rock', 'country', 'rnb'];
 
+    const STARTING_YEAR = 1900; // Do one year at a time (per decade) to reduce load on server
+
     for (let genre of genres) {
         // TODO: use date.year instead of "2024"
         for (let year = 1900; year <= 2024; year += 10) {
