f899285101194b2edff7b1ed0367027af52d6607
Rich Clingman <richclingman@gmail.com>
2024-07-12 20:31:41 -0500

    Add genre buttons on left; cause alert() when clicked

--- pages/jukebox.vue
+++ pages/jukebox.vue
@@ -3,9 +3,11 @@
     <div class="left-bar">
       <img src="/img/jukebox_8676230.png" alt="Image of jukebox" />
       <div class="brand">Digital Jukebox</div>
+
+      <button v-for="genre in genres" class="genre" @click="doDecade(genre)">{{genre}}</button>
     </div>
     <div class="fullwidth">
-      <div v-for="decade in decades" class="box">{{ decade }}</div>
+      <button v-for="decade in decades" class="decade" @click="doDecade(decade)">{{ decade }}</button>
     </div>
   </div>
 </template>
@@ -15,8 +17,15 @@ definePageMeta({
   layout: 'clean'
 })
 
-const decades = ref([1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020]);
+const doDecade = (decade) => {
+  alert(`decade ${decade}`)
+}
+const doGenre = (genre) => {
+  alert(`genre ${genre}`)
+}
 
+const decades = ref([1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020]);
+const genres = ref(['Top 100', 'Country', 'Rock & Roll', 'R & B']);
 </script>
 
 <style scoped lang="sass">
@@ -30,6 +39,10 @@ $left-bar-color: burlywood
   height: 100vw
   width: 100hw
 
+.left-bar
+  width: 225px
+  background-color: $left-bar-color
+
   img
     margin-top: 5px
   .brand
@@ -37,10 +50,17 @@ $left-bar-color: burlywood
     text-align: center
     font-weight: bolder
     font-size: 20px
-
-.left-bar
-  width: 225px
-  background-color: $left-bar-color
+  .genre
+    width: 100%
+    height: 50px
+    background-color: $box-background-color
+    border: 1px solid $box-border-color
+    box-sizing: border-box
+    text-align: center
+    vert-align: middle
+    font-weight: bold
+    font-size: 20px
+    padding: 0
 
 .fullwidth
   flex: 1
@@ -50,10 +70,11 @@ $left-bar-color: burlywood
   height: 50px
   background-color: $background-color
 
-  .box
+  .decade
     flex: 1
     height: 100%
     background-color: $box-background-color
     border: 1px solid $box-border-color
     box-sizing: border-box
+    font-weight: bold
 </style>
